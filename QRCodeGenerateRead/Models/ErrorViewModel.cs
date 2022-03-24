708697b8df6aa0f10232e8f2db7fb03f17be5551
ParvizHacili <parviz.hajili@gmail.com>
2022-03-24 15:58:36 +0400

    Add project files.

---
+++ QRCodeGenerateRead/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QRCodeGenerateRead.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
